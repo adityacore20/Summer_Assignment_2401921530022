41e66dd3d49fb476dae0ee502774d3fe63f35ac0
adityacore20 <adityamahore709@gmail.com>
2026-06-02 09:16:23 +0530

    Day_1 done

---
+++ Week_1/Day_1/remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+class Solution{
+
+    public:
+        int removeDuplicates(vector<int>& nums){
+            if(nums.empty()) return 0;
+            int k=0;
+            for(int i=1;i<nums.size();i++)
+            {
+                if(nums[i]!=nums[k]){
+                    k++;
+                    nums[k]=nums[i];
+
+                }
+            }
+            return k+1;
+
+        }
+};
