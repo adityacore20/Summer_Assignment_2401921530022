80818953a4f7187f3d290344717507df0325770d
adityacore20 <adityamahore709@gmail.com>
2026-06-09 00:00:03 +0530

    Week_2 Day_1 done

---
+++ Week_2/Day_1/ransom_note.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        
+        int counts[26] = {0};
+        
+        for (char c : magazine) {
+            counts[c - 'a']++;
+        }
+        
+        for (char c : ransomNote) {
+            counts[c - 'a']--;
+            
+            if (counts[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
