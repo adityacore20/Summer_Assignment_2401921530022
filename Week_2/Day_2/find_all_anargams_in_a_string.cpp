6eda77761fbe6569608f37c62c095f3fa84c7122
adityacore20 <adityamahore709@gmail.com>
2026-06-09 18:08:38 +0530

    Week_2 Day_2 done

---
+++ Week_2/Day_2/find_all_anargams_in_a_string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> result;
+        int s_len = s.length();
+        int p_len = p.length();
+        
+        if (s_len < p_len) return result;
+        
+        vector<int> p_freq(26, 0);
+        vector<int> window_freq(26, 0);
+        
+        for (int i = 0; i < p_len; ++i) {
+            p_freq[p[i] - 'a']++;
+            window_freq[s[i] - 'a']++;
+        }
+        
+        if (p_freq == window_freq) {
+            result.push_back(0);
+        }
+        
+        for (int i = p_len; i < s_len; ++i) {
+            window_freq[s[i] - 'a']++;
+            window_freq[s[i - p_len] - 'a']--;
+            
+            if (p_freq == window_freq) {
+                result.push_back(i - p_len + 1);
+            }
+        }
+        
+        return result;
+    }
+};
